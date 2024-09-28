57ff4777e8a30d2571436fc4be31ae3e7ceb39c6
extr3y <extr3y@duck.com>
2024-09-28 16:47:01 +0200

    Update painter.py
    Added comments

--- src/painter.py
+++ src/painter.py
@@ -10,16 +10,21 @@ class ArmPainter:
         self.arm = arm
         self.center = center
 
+        # trigger interactive mode to enable repainting
         plt.ion()
         self.fig, self.ax = plt.subplots()
 
         self.line = self.setup()
 
+    """
+    Initial setup; summons an arm parallel to the X axis
+    Returns mutable plot data
+    """
     def setup(self):
         x, y = self.point_array_to_coord_lists()
         line, = self.ax.plot(x, y)
 
-        # Define plot limits
+        # defines plot limits
         size = 10
         self.ax.set_xlim(-size, size)
         self.ax.set_ylim(-size, size)
@@ -27,6 +32,10 @@ class ArmPainter:
 
         return line
 
+    """
+    Calculates the position of joints and the end effector (point designed to make contact with the target)
+    Returns: list of x and y coordinates used for plotting
+    """
     def point_array_to_coord_lists(self):
         x = []
         y = []
@@ -40,7 +49,9 @@ class ArmPainter:
         
         return (x, y)
     
-
+    """
+    Refreshes the plot
+    """
     def update(self):
 
         x, y = self.point_array_to_coord_lists()
@@ -48,21 +59,32 @@ class ArmPainter:
         self.line.set_xdata(x)
         self.line.set_ydata(y)
 
-        # Redraw the plot
-        self.ax.relim()  # Recalculate limits
-        self.ax.autoscale_view(True, True, True)  # Update axes based on new data
+        # recalculates plot's limits
+        self.ax.relim()
+        # updates axes based on new data
+        self.ax.autoscale_view(True, True, True)  
+        # redraws the plot
         plt.draw()
     
-        # Pause for a second to simulate real-time updating
+        # pause for a second to simulate real-time updating
         plt.pause(0.1)
 
-
+    """
+    Displays the plot
+    """
     def show_canvas(self):
         plt.show()
 
+    """
+    Plots a single point of interest (eg. the target)
+    Accepts the point's position, color and size values 
+    """
     def markup(self, point: Point, color: str, s = 35):
         self.ax.scatter(point.x, point.y, s = s, color = color)
 
+    """
+    Shuts down plotting
+    """
     def close(self):
         plt.ioff()
         plt.close()
